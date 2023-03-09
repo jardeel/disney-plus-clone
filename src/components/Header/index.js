eca053d44105bb4a23e2d3012c19fa38ae20a678
Jardel Sousa <jardeelsousa@hotmail.com>
2023-03-08 21:40:36 -0300

    Initial Commit Add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,38 @@
+import { Nav, Logo, NavMenu, UserImg } from './styles';
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg"/>
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg"/>
+          <span>Home</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>Search</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>Watchlist</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>Originals</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>Movies</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>Series</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80"/>
+    </Nav>
+  )
+}
+
+export default Header;
