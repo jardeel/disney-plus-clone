3255be09f6f52d3699b220f5bf2189c836b29b8d
Jardel Sousa <jardeelsousa@hotmail.com>
2023-04-23 11:23:59 -0300

    Add Viewers

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import { Container } from './styles';
 import ImgSlider from '../ImgSlider';
+import Viewers from '../Viewers';
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   )
 }
---
+++ src/components/Viewers/index.js
@@ -0,0 +1,26 @@
+import { Container, Wrap } from './styles';
+
+
+function Viewers() {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png"/>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png"/>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png"/>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png"/>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png"/>
+      </Wrap>
+    </Container>
+  )
+}
+
+export default Viewers;
